5bab035ca6ca9f28fb0d1f26d9dc8a84f72d6c9d
AntonZaychenko <antonyzaychenko30012001@gmail.com>
2022-08-02 14:40:07 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,31 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryContainer = document.querySelector(".gallery");
-const galleryMarkup = createItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+const myGallery = document.querySelector('.gallery')
+const galleryMarkup = createContainer(galleryItems)
+myGallery.insertAdjacentHTML('beforeend', galleryMarkup)
 
-function createItemsMarkup(item) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<ul class="gallery">
-      <li>
-      <a class="gallery__item" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"          
-          alt="${description}"
-        />
-      </a>
-      </li>
-     
-    </ul>`;
-    })
-    .join("");
+
+
+function createContainer() {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `
+    }).join('')
 }
 
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
-  captionType: "alt",
-});
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+  });
